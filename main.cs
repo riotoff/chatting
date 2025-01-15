8bee3a29db40cbb8a29d29f20b192d4b8f574dd3
riotoff <daxkod3243@gmail.com>
2025-01-15 22:10:50 +0300

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,8 @@
+// GNU General Public License 3.0
+/*
+ * Anonymous Messenger written on C#
+ * Author: https://github.com/riotoff/
+ * Repository: https://github.com/riotoff/chatting_messenger/
+*/
+
+// Soon
